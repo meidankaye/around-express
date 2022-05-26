5677346c16cbdf8935ba32966f6a1ea475a4ca07
meidankaye <87809871+meidankaye@users.noreply.github.com>
2022-05-27 00:50:57 +0300

    fix: need to fix getting and creating users

--- controllers/users.js
+++ controllers/users.js
@@ -1,36 +1,22 @@
 const User = require('../models/user');
 
-const getUsers = async (req, res) => {
-  try {
-    const users = await User.find({});
-
-    res.send(users);
-  } catch (error) {
-    res.status(500).send({ message: 'An error has occurred on the server' });
-  }
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send(users))
+    .catch(() => res.status(500).send({ message: 'An error has occurred on the server' }));
 };
 
-const getUserById = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.userId);
-
-    if (!user) {
-      res.status(404).send({ message: 'User ID not found' });
-    }
-    res.status(200).send(user);
-  } catch (error) {
-    res.status(500).send({ message: 'An error has occurred on the server' });
-  }
+const getUserById = (req, res) => {
+  User.findById(req.params.userId)
+    .orFail()
+    .then((user) => res.send(user))
+    .catch(() => res.status(500).send({ message: 'An error has occurred on the server' }));
 };
 
-const createUser = async (req, res) => {
-  try {
-    const newUser = await User.create(req.body);
-
-    res.send(newUser);
-  } catch (error) {
-    res.status(500).send({ message: 'An error has occurred on the server' });
-  }
+const createUser = (req, res) => {
+  User.create(req.body)
+    .then((newUser) => res.send(newUser))
+    .catch(() => res.status(500).send({ message: 'An error has occurred on the server' }));
 };
 
 module.exports = {
