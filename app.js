348a8ada9e079623f914da3b57214fb6aee2378e
meidankaye <87809871+meidankaye@users.noreply.github.com>
2022-07-09 17:08:05 +0300

    feat: project 15 backend

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -7,18 +8,12 @@ const cardsRouter = require('./routes/cards');
 
 mongoose.connect('mongodb://localhost:27017/aroundb');
 
+app.use(cors());
+app.options('*', cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '6290e407637e876c7bb47c34',
-  };
-
-  next();
-});
-
-app.use(usersRouter);
-app.use(cardsRouter);
+app.use('/', usersRouter);
+app.use('/', cardsRouter);
 
 app.use('*', (req, res) => {
   res.status(404).send({ message: 'Requested resource not found' });
